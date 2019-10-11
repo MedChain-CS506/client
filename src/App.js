9f32cae5ad5ade6976eb6b594cfe96df5f9515f1
Brennan Fife <brennanfife@Brennans-MacBook-Pro.local>
2019-10-10 23:42:06 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Landing from './pages/Landing'
 import Dashboard from './pages/Dashboard'
+import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <div>
-      <Landing />
-      <Dashboard />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Landing} />
+        <Route exact path='/dashboard' component={Dashboard} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
