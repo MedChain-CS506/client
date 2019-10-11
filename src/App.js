27416ab1b6da77aac5908f48a40989416d8c51bb
Brennan Fife <brennanfife@Brennans-MacBook-Pro.local>
2019-10-10 21:18:34 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Landing from './pages/Landing'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
     <div>
+      <Landing />
+      <Dashboard />
     </div>
   );
 }
