eba49042378aff6dddbe300d3ebe7b1b80f0e9a0
Brennan Fife <brennanfife@Brennans-MacBook-Pro.local>
2019-10-19 11:29:38 -0500

    commit

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-const Landing = () => {
-    return (
-        <div>
-            <h1>Landing</h1>
-        </div>
-    )
-}
+import Fab from '@material-ui/core/Fab';
+
+import HomeIcon from '@material-ui/icons/Home';
+
+import GitHubCircleIcon from 'mdi-material-ui/GithubCircle';
+
+import EmptyState from '../EmptyState';
+
+class Landing extends Component {
+    render() {
+  
+      // Properties
+      const { signedIn } = this.props;
+  
+      if (signedIn) {
+        return (
+          <EmptyState
+            icon={<HomeIcon color="action" />}
+            title="Home"
+          />
+        );
+      }
+  
+      return (
+        <EmptyState
+          title='MedChain'
+          description="The robust health file application"
+          button={
+            <Fab color="secondary" href="#" rel="noopener noreferrer" target="_blank" variant="extended">
+              <GitHubCircleIcon />
+                Repo
+            </Fab>
+          }
+        />
+      );
+    }
+  }
 
 export default Landing
