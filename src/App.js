777a5ba838ac96be779a9d73e85bef6127d0c9ba
Brennan Fife <brennanfife@Brennans-MacBook-Pro.local>
2019-10-26 14:49:47 -0500

    beginning to add a mounting function for sign in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { ThemeProvider } from '@material-ui/core/styles';
 
@@ -24,13 +24,143 @@ function App() {
   const [userData, setUserData] = useState(null)
   // const [performingAction, setPerformingAction]= useState(false)
 
-  const [signedIn] = useState(true);
-  const [ready] = useState(true);
+  const [signedIn, setSignedIn] = useState(false);
+  const [ready, setReady] = useState(true);
   const [signUpDialog, setSignUpDialog] = useState(false)
   const [signInDialog, setSignInDialog] = useState(false)
   const [settingsDialog, setSettingsDialog] = useState(false)
 
   const [signOutDialog, setSignOutDialog] = useState(false) //!ADDED
+
+
+  const [mounted, setMounted] = useState(false) //!FOR THE USEEFFECT BELOW
+
+
+  useEffect(() => {
+    setMounted(true)
+
+    const removeAuthStateChangedObserver = auth.onAuthStateChanged((user) => {
+      //* if there is no user...
+      if (!user) {
+        // if (removeReferenceListener) {
+        //   removeReferenceListener()
+        // }
+
+        if (mounted) {
+          setUser(null)
+          setUserData(null)
+          setSignedIn(false)
+          setReady(true)
+        }
+
+        return
+
+      }
+
+      const uid = user.uid
+
+      //* if there is no uid...
+      if (!uid) {
+        // if (removeReferenceListener) {
+        //   removeReferenceListener()
+        // }
+
+        if (mounted) {
+          setUser(null)
+          setUserData(null)
+          setSignedIn(false)
+          setReady(true)
+        }
+
+        return
+
+      }
+
+      const reference = firestore.collection('users').doc(uid)
+
+      //* if there is no reference...
+      if (!reference) {
+        if (this.removeReferenceListener) {
+          this.removeReferenceListener();
+        }
+
+        if (mounted) {
+          setUser(null)
+          setUserData(null)
+          setSignedIn(false)
+          setReady(true)
+        }
+
+        return
+
+      }
+
+      const removeReferenceListener = reference.onSnapshot((snapshot) => { //!ADDED CONST HERE INSTEAD OF THIS
+        if (!snapshot.exists) {
+          if (removeReferenceListener) {
+            removeReferenceListener();
+          }
+
+          if (mounted) {
+            setUser(null)
+            setUserData(null)
+            setSignedIn(false)
+            setReady(true)
+          }
+
+          return
+
+        }
+
+        const data = snapshot.data();
+
+        if (!data) {
+          if (removeReferenceListener) {
+            removeReferenceListener();
+          }
+
+          if (mounted) {
+            setUser(null)
+            setUserData(null)
+            setSignedIn(false)
+            setReady(true)
+          }
+
+          return
+
+        }
+
+        if (mounted) {
+          setUser(user)
+          setUserData(data)
+          setSignedIn(true)
+          setReady(true)
+        }
+
+      })
+    })
+
+    //!FOR THE componentWillUnmount
+    return () => {
+      if (removeAuthStateChangedObserver) {
+        removeAuthStateChangedObserver();
+      }
+  
+      // if (removeReferenceListener) {
+      //   removeReferenceListener();
+      // }
+  
+      setMounted(false);
+    }
+
+  }, [mounted]) 
+
+
+
+
+
+
+
 
   const signOut = () => {
     authentication.signOut().then(() => setSignOutDialog(false))
