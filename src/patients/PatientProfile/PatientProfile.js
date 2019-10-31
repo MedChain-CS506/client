9fd140f5c211fb58c8a9a724912cd50498457ada
Brennan Fife <brennanfife@Brennans-MacBook-Pro.local>
2019-10-30 19:36:44 -0500

    commit

--- src/patients/PatientProfile/PatientProfile.js
+++ src/patients/PatientProfile/PatientProfile.js
@@ -1,12 +1,48 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+      flexGrow: 1
+    },
+    paper: {
+      m: 'auto'
+    }
+});
 
 const PatientProfile = () => {
+    const classes = useStyles();
     return (
-        <div>
-           <h1>Patient Profile</h1> 
-           <h3>Patient Stats</h3>
-           <h3>Prescriptions</h3>
-           <h3>Recent Activity - Upcoming Appointments, Medical Operations</h3>
+        <div className={classes.root}>
+            <Grid container spacing={3}>
+                <Grid item xs={6}>
+                    <Typography variant='h2'>Patient Name</Typography>
+                    <Typography variant='h5'>Aadhaar #</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                    <Typography variant='h6'>Date of Birth</Typography>
+                    <Typography variant='h6'>Sex</Typography>
+                    <Typography variant='h6'>Weight</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                    <Paper className={classes.paper}>
+                        <Typography variant='h5'>Known Allergies</Typography>
+                    </Paper>
+                </Grid>
+                <Grid item xs={6}>
+                    <Paper className={classes.paper}>
+                        <Typography variant='h5'>Prescriptions</Typography> 
+                    </Paper>
+                </Grid>
+                <Grid item xs={12}>
+                    <Paper className={classes.paper}>
+                        <Typography variant='h5'>Recent Activity - Upcoming Appointments, Medical Operations</Typography> 
+                    </Paper>
+                </Grid>
+            </Grid>
         </div>
     )
 }
